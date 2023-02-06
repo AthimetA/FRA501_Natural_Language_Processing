15f1bf68f690383fb9bf44a5afed303dc6b05b4a
Athimet Aiewcharoen <mobiltiy2604@gmail.com>
2023-02-06 12:25:29 +0700

    Three fully connected model

---
+++ Homework/HW1/Model/modelConfig.py
@@ -0,0 +1,12 @@
+class FFNNConfig():
+    def __init__(self):
+        pass
+        
+    # Training parameters
+    verbose = 1
+    batch_size = 512
+    epochs = 20
+
+    # Model parameters
+    input_dim = 21
+    output_dim = 1
